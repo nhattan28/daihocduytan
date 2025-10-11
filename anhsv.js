6f9d379c36da869378a3370837c531bbdde8cf78
Tân <tan2810203@gmail.com>
2025-10-11 20:07:13 +0700

    Update anhsv.js

--- anhsv.js
+++ anhsv.js
@@ -1,4 +1,7 @@
-// Chặn một số phím tắt phổ biến 
+document.addEventListener('contextmenu', function (e) {
+  e.preventDefault();
+});
+
 document.addEventListener('keydown', function (e) {
   if (e.key === 'F12' || (e.ctrlKey && e.shiftKey && (e.key === 'I' || e.key === 'J' || e.key === 'C'))) {
     e.preventDefault();
